b7c2105aaf32fb2323392978e5e8b5da1acce073
wojciechlasak <wojciech.lasak@outlook.com>
2021-05-04 17:04:35 +0200

    added content, added poweredby

--- index.php
+++ index.php
@@ -2,55 +2,57 @@
 <?php
   $links = [
     [
-      'title'    => "lorem",
-      'subtitle' => 'lorem ipsum',
+      'title'    => "seminaria",
+      'subtitle' => 'the stack',
       'url'      => '',
     ],
     [
-      'title'    => "lorem",
-      'subtitle' => 'lorem ipsum',
+      'title'    => "txt",
+      'subtitle' => 'nooskop',
       'url'      => '',
     ],
     [
-      'title'    => "lorem",
-      'subtitle' => 'lorem ipsum',
+      'title'    => "wydarzenia",
+      'subtitle' => '',
       'url'      => '',
     ],
     [
-      'title'    => "lorem",
-      'subtitle' => 'lorem ipsum',
+      'title'    => "wyprawy",
+      'subtitle' => 'poręba 2020<br /> katowice 2018',
       'url'      => '',
     ],
     [
-      'title'    => "lorem",
-      'subtitle' => 'lorem ipsum',
+      'title'    => "projekty",
+      'subtitle' => '',
       'url'      => '',
     ],
     [
-      'title'    => "lorem",
-      'subtitle' => 'lorem ipsum',
-      'url'      => '',
-    ],
-    [
-      'title'    => "lorem",
-      'subtitle' => 'lorem ipsum',
+      'title'    => "inne",
+      'subtitle' => 'merch',
       'url'      => '',
     ],
   ];
 ?>
-<div id="container-out" class="flex">
-  <div id="container-main">
+<div id="container-out" class="flex flex-1050">
+  <div id="container-main" class="col2">
     <img id="logo" src='./media/logo.png' />
     <a id="twitter" href="" target="_blank"><img src="./media/twitter.png"
         alt="" /></a>
     <div id="container-time">czas</div>
   </div>
-  <div id="container-links">
+  <div id="container-links" class="col2">
     <div id="newsletter" class="container-link">news</div>
+    <div class="container-link">
+      <a href="<?=$link['url']?>">
+        <div class="link-title">powered by <br /></div>
+        GRUPA <br />
+        ROBOCZA
+      </a>
+    </div>
     <?php foreach ($links as $link) {?>
     <div class="container-link">
       <a href="<?=$link['url']?>">
-        <div class="link-title"><?=$link['title']?></div>
+        <div class="link-title--uppercase"><?=$link['title']?></div>
         <div class="link-subtitle"><?=$link['subtitle']?></div>
       </a>
     </div>
