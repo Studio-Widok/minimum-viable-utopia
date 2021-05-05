4124ad41883c9fe728987353d4b68db37a12ff53
wojciechlasak <wojciech.lasak@outlook.com>
2021-05-05 10:45:43 +0200

    changed links stucture, deleted hover for container, added urls

--- index.php
+++ index.php
@@ -1,60 +1,78 @@
 <?php include 'header.php';?>
 <?php
-  $links = [
+  $items = [
     [
-      'title'    => "seminaria",
-      'subtitle' => 'the stack',
-      'url'      => '',
+      'title' => "seminaria",
+      'links' => [
+        [
+          'text' => 'the stack',
+          'url'  => 'https://mvu.pl/the_stack/',
+        ],
+      ],
     ],
     [
-      'title'    => "txt",
-      'subtitle' => 'nooskop',
-      'url'      => '',
+      'title' => "txt",
+      'links' => [
+        [
+          'text' => 'nooskop',
+          'url'  => 'https://nooskop.mvu.pl/',
+        ],
+      ],
     ],
     [
-      'title'    => "wydarzenia",
-      'subtitle' => '',
-      'url'      => '',
+      'title' => "wydarzenia",
+      'links' => [],
     ],
     [
-      'title'    => "wyprawy",
-      'subtitle' => 'poręba 2020<br /> katowice 2018',
-      'url'      => '',
+      'title' => "wyprawy",
+      'links' => [
+        [
+          'text' => 'poręba 2020',
+          'url'  => 'https://mvu.pl/wyprawa/',
+        ],
+        [
+          'text' => 'katowice 2018',
+          'url'  => 'https://wyprawa.robocza.org/',
+        ],
+      ],
     ],
     [
-      'title'    => "projekty",
-      'subtitle' => '',
-      'url'      => '',
+      'title' => "projekty",
+      'links' => [],
     ],
     [
-      'title'    => "inne",
-      'subtitle' => 'merch',
-      'url'      => '',
+      'title' => "inne",
+      'links' => [
+        [
+          'text' => 'merch',
+          'url'  => 'https://odziez.robocza.org/',
+        ],
+      ],
     ],
   ];
 ?>
 <div id="container-out" class="flex flex-1050">
   <div id="container-main" class="col2">
     <img id="logo" src='./media/logo.png' />
-    <a id="twitter" href="" target="_blank"><img src="./media/twitter.png"
-        alt="" /></a>
+    <a id="twitter" href="https://twitter.com/mv_utopia" target="_blank"><img
+        src="./media/twitter.png" alt="" /></a>
     <div id="container-time">czas</div>
   </div>
   <div id="container-links" class="col2">
     <div id="newsletter" class="container-link">news</div>
     <div class="container-link">
-      <a href="<?=$link['url']?>">
-        <div class="link-title">powered by <br /></div>
-        GRUPA <br />
-        ROBOCZA
+      <a href="http://grupa.robocza.org/" target="_blank">
+        <img src="./media/gruparobocza.svg" />
       </a>
     </div>
-    <?php foreach ($links as $link) {?>
+    <?php foreach ($items as $item) {?>
     <div class="container-link">
-      <a href="<?=$link['url']?>">
-        <div class="link-title--uppercase"><?=$link['title']?></div>
-        <div class="link-subtitle"><?=$link['subtitle']?></div>
+      <div class="links-title--uppercase"><?=$item['title']?></div>
+      <?php foreach ($item['links'] as $link) {?>
+      <a href="<?=$link['url']?>" target="_blank">
+        <div class="link-text"><?=$link['text']?></div>
       </a>
+      <?php }?>
     </div>
     <?php }?>
   </div>
